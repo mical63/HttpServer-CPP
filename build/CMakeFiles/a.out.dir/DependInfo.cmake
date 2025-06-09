feaa77d1ad11782a7bea03de488538a4a6b5e530
mical63 <15932656692@163.com>
2025-06-09 22:28:42 +0800

    cmake 来喽

---
+++ build/CMakeFiles/a.out.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mical/HttpSevver/HttpServer-CPP/src/Buffer.cpp" "CMakeFiles/a.out.dir/src/Buffer.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/Buffer.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/Channel.cpp" "CMakeFiles/a.out.dir/src/Channel.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/Channel.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/Dispatcher.cpp" "CMakeFiles/a.out.dir/src/Dispatcher.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/Dispatcher.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/EpollDispatcher.cpp" "CMakeFiles/a.out.dir/src/EpollDispatcher.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/EpollDispatcher.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/EventLoop.cpp" "CMakeFiles/a.out.dir/src/EventLoop.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/EventLoop.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/HttpRequest.cpp" "CMakeFiles/a.out.dir/src/HttpRequest.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/HttpRequest.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/HttpResponse.cpp" "CMakeFiles/a.out.dir/src/HttpResponse.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/HttpResponse.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/PollDispatcher.cpp" "CMakeFiles/a.out.dir/src/PollDispatcher.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/PollDispatcher.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/SelectDispatcher.cpp" "CMakeFiles/a.out.dir/src/SelectDispatcher.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/SelectDispatcher.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/TcpConnection.cpp" "CMakeFiles/a.out.dir/src/TcpConnection.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/TcpConnection.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/TcpServer.cpp" "CMakeFiles/a.out.dir/src/TcpServer.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/TcpServer.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/ThreadPool.cpp" "CMakeFiles/a.out.dir/src/ThreadPool.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/ThreadPool.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/WorkerThread.cpp" "CMakeFiles/a.out.dir/src/WorkerThread.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/WorkerThread.cpp.o.d"
+  "/home/mical/HttpSevver/HttpServer-CPP/src/main.cpp" "CMakeFiles/a.out.dir/src/main.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/a.out.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.out.dir/src/Buffer.cpp.o"
+  "CMakeFiles/a.out.dir/src/Buffer.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/Channel.cpp.o"
+  "CMakeFiles/a.out.dir/src/Channel.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/Dispatcher.cpp.o"
+  "CMakeFiles/a.out.dir/src/Dispatcher.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/EpollDispatcher.cpp.o"
+  "CMakeFiles/a.out.dir/src/EpollDispatcher.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/EventLoop.cpp.o"
+  "CMakeFiles/a.out.dir/src/EventLoop.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/HttpRequest.cpp.o"
+  "CMakeFiles/a.out.dir/src/HttpRequest.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/HttpResponse.cpp.o"
+  "CMakeFiles/a.out.dir/src/HttpResponse.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/PollDispatcher.cpp.o"
+  "CMakeFiles/a.out.dir/src/PollDispatcher.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/SelectDispatcher.cpp.o"
+  "CMakeFiles/a.out.dir/src/SelectDispatcher.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/TcpConnection.cpp.o"
+  "CMakeFiles/a.out.dir/src/TcpConnection.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/TcpServer.cpp.o"
+  "CMakeFiles/a.out.dir/src/TcpServer.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/ThreadPool.cpp.o"
+  "CMakeFiles/a.out.dir/src/ThreadPool.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/WorkerThread.cpp.o"
+  "CMakeFiles/a.out.dir/src/WorkerThread.cpp.o.d"
+  "CMakeFiles/a.out.dir/src/main.cpp.o"
+  "CMakeFiles/a.out.dir/src/main.cpp.o.d"
+  "a.out"
+  "a.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
